7c50be9dc9682e433c18f05219771c0629457a72
FeuFeve <clement-potin@outlook.com>
2021-02-02 22:25:56 +0100

    Update Combat report parser.py (add French support, fix minor bugs and imprecisions)

--- Python/Combat report parser.py
+++ Python/Combat report parser.py
@@ -1,19 +1,22 @@
+import io
+
+
 # **************** GLOBAL VARIABLES **************** #
 
-young_dwarves = [8, 3, 2, "Young dwarves", "Young dwarf"]
-dwarves = [10, 5, 4, "Dwarves", "Dwarf"]
-top_dwarves = [13, 7, 6, "Top dwarves", "Top dwarf"]
-young_soldiers = [16, 10, 9, "Young soldiers", "Young soldier"]
-soldiers = [20, 15, 14, "Soldiers", "Soldier"]
-doorkeepers = [30, 1, 25, "Doorkeepers", "Doorkeeper"]
-top_doorkeepers = [40, 1, 35, "Top doorkeepers", "Top doorkeeper"]
-fire_ants = [10, 30, 15, "Fire ants", "Fire ant"]
-top_fire_ants = [12, 35, 18, "Top fire ants", "Top fire ant"]
-top_soldiers = [27, 24, 23, "Top soldiers", "Top soldier"]
-tanks = [35, 55, 1, "Tanks", "Tank"]
-top_tanks = [50, 80, 1, "Top tanks", "Top tanks"]
-killers = [50, 50, 50, "Killers", "Killer"]
-top_killers = [55, 55, 55, "Top killers", "Top killer"]
+young_dwarves = [8, 3, 2, "Young dwarves", "Young dwarf", "Jeunes Soldates Naines", "Jeune Soldate Naine"]
+dwarves = [10, 5, 4, "Dwarves", "Dwarf", "Soldates Naines", "Soldate Naine"]
+top_dwarves = [13, 7, 6, "Top dwarves", "Top dwarf", "Naines d’Elites", "Naine d’Elite"]
+young_soldiers = [16, 10, 9, "Young soldiers", "Young soldier", "Jeunes Soldates", "Jeune Soldate"]
+soldiers = [20, 15, 14, "Soldiers", "Soldier", "Soldates", "Soldate"]
+doorkeepers = [30, 1, 25, "Doorkeepers", "Doorkeeper", "Concierges", "Concierge"]
+top_doorkeepers = [40, 1, 35, "Top doorkeepers", "Top doorkeeper", "Concierges d’élites", "Concierge d’élite"]
+fire_ants = [10, 30, 15, "Fire ants", "Fire ant", "Artilleuses", "Artilleuse"]
+top_fire_ants = [12, 35, 18, "Top fire ants", "Top fire ant", "Artilleuses d’élites", "Artilleuse d’élite"]
+top_soldiers = [27, 24, 23, "Top soldiers", "Top soldier", "Soldates d’élites", "Soldate d’élite"]
+tanks = [35, 55, 1, "Tanks", "Tank", "Tanks", "Tank"]
+top_tanks = [50, 80, 1, "Top tanks", "Top tanks", "Tanks d’élites", "Tank d’élite"]
+killers = [50, 50, 50, "Killers", "Killer", "Tueuses", "Tueuse"]
+top_killers = [55, 55, 55, "Top killers", "Top killer", "Tueuses d’élites", "Tueuse d’élite"]
 
 antz_units = [young_dwarves, dwarves, top_dwarves, young_soldiers, soldiers, doorkeepers, top_doorkeepers,
               fire_ants, top_fire_ants, top_soldiers, tanks, top_tanks, killers, top_killers]
@@ -34,17 +37,23 @@ def format_number(number):
 def get_readable_army(army):
     first = True
     readable_army = ""
+    offset = 0
+    if not is_english:
+        offset = 2
     for i in range(14):
         if army[i] > 0:
             if not first:
                 readable_army += ", "
             first = False
             if army[i] == 1:
-                readable_army += format_number(army[i]) + " " + antz_units[i][4]
+                readable_army += format_number(army[i]) + " " + antz_units[i][4 + offset]
             else:
-                readable_army += format_number(army[i]) + " " + antz_units[i][3]
+                readable_army += format_number(army[i]) + " " + antz_units[i][3 + offset]
     if first:
-        readable_army += "None"
+        if is_english:
+            readable_army += "None"
+        else:
+            readable_army += "Aucune"
     return readable_army + "."
 
 
@@ -55,9 +64,13 @@ def get_readable_army(army):
 def parse_troops(line_to_parse):
     army = [0] * 14
     units = line_to_parse.split(", ")
+    units[-1] = units[-1].split(".")[0]
+    offset = 0
+    if not is_english:
+        offset = 2
     for unit in units:
         for j in range(14):
-            if antz_units[j][3] in unit or antz_units[j][4] in unit:
+            if unit.endswith(antz_units[j][3 + offset]) or unit.endswith(antz_units[j][4 + offset]):
                 army[j] = int(''.join(c for c in unit if c.isdigit()))
                 break
     return army
@@ -106,15 +119,27 @@ def get_first_turn_dead_army(army, dead_troops):
 # Calculate the attacker shields level
 def calculate_attacker_shields():
     if defender_weapons == "?" or first_turn_defender_damages < 2200:
-        print("/!\\ Defender's damages are too low to calculate Attacker's shields level")
+        if is_english:
+            print("/!\\ Defender's damages are too low (<2000) to calculate attacker's Shields level")
+        else:
+            print("/!\\ Les dégâts du défenseur sont trop faibles (<2000) pour calculer le niveau de Boucliers de "
+                  "l'attaquant")
         return "?"
     elif first_turn_defender_damages < 5000:
-        print("/!\\ Defender's damages are low. Attacker's shields level might not be correct")
+        if is_english:
+            print("/!\\ Defender's damages are low (<5000). Attacker's Shields level might not be correct")
+        else:
+            print("/!\\ Les dégâts du défenseur sont faibles (<5000). Le niveau de Boucliers de l'attaquant pourrait "
+                  "ne pas être correct")
 
     first_turn_dead_army, total_life_lost = get_first_turn_dead_army(attacker_army, first_turn_attacker_dead)
 
     if first_turn_dead_army == attacker_army:
-        print("/!\\ Attacker's army got OS, can't calculate attacker's shields level")
+        if is_english:
+            print("/!\\ Attacker's army got OS, can't calculate attacker's Shields level")
+        else:
+            print("/!\\ L'armée de l'attaquant a été tuée en 1 tour, il n'est donc pas possible de calculer le niveau "
+                  "de Boucliers de l'attaquant")
         return "?"
     else:
         return int((round(first_turn_defender_damages / total_life_lost, 1) - 1) * 10)
@@ -128,29 +153,42 @@ def calculate_attacker_shields():
 #   place_level: The defender dome or nest level, if the fight occurs in dome or nest
 def calculate_defender_shields_and_place_level(place):
     if attacker_weapons == "?" or first_turn_attacker_damages < 5000:
-        print("/!\\ Attacker's damages are too low to calculate Defender's shields/dome/nest level")
+        if is_english:
+            print("/!\\ Attacker's damages are too low (<5000) to calculate defender's Shields/Dome/Nest levels")
+        else:
+            print("/!\\ Les dégâts de l'attaquant sont trop faibles (<5000) pour calculer les niveaux de "
+                  "Boucliers/Dôme/Loge de défenseur")
         return "?", "?"
     elif first_turn_attacker_damages < 10000:
-        print("/!\\ Attacker's damages are low. Defender's shields/dome/nest level might not be correct")
+        if is_english:
+            print("/!\\ Attacker's damages are low (<10,000). Defender's Shields/Dome/Nest levels might not be correct")
+        else:
+            print("/!\\ Les dégâts de l'attaquant sont faibles (<10 000). Les niveaux de Boucliers/Dôme/Loge du "
+                  "défenseur pourraient ne pas être corrects")
 
     first_turn_dead_army, total_life_lost = get_first_turn_dead_army(defender_army, first_turn_defender_dead)
 
     if first_turn_dead_army == defender_army:
-        print("/!\\ Defender's army got OS, can't calculate defender's shields/anthill/nest levels")
+        if is_english:
+            print("/!\\ Defender's army got OS, can't calculate defender's Shields/Dome/Nest levels")
+        else:
+            print("/!\\ L'armée du défenseur a été tuée en 1 tour, il n'est donc pas possible de calculer les niveaux "
+                  "de Boucliers/Dôme/Loge du défenseur")
         return "?", "?"
     else:
         if place == "Hunting Field":
             return int((round(first_turn_attacker_damages / total_life_lost, 1) - 1) * 10)
 
-        life_multiplier = round(first_turn_attacker_damages / total_life_lost, 1)
+        # Double round is needed for cases like "round(4.4499, 1) = 4.4" where we want 4.5
+        life_multiplier = round(first_turn_attacker_damages / total_life_lost, 2)
         shields = defender_weapons  # Supposition : shield == weapons, determine dome/nest level with that
         place_life_multiplier = life_multiplier - (shields / 10)
         place_level = 0
         if place == "Anthill":
-            place_level = int((place_life_multiplier - 1 - 0.1) / 0.05)
+            place_level = int(round((place_life_multiplier - 1 - 0.1) / 0.05))
         elif place == "Nest":
             place_bonus = place_life_multiplier - 1 - 0.3
-            place_level = int(place_bonus / 0.15)
+            place_level = int(round(place_bonus / 0.15, 2))
             modulus = round(place_bonus % 0.15, 2)
             if modulus == 0.05:
                 place_level += 1
@@ -164,11 +202,12 @@ def calculate_defender_shields_and_place_level(place):
 # ********************** MAIN ********************** #
 
 # The combat report from Antzzz
-combat_report = open("combat-report.txt")
+combat_report = io.open("combat-report.txt", mode="r", encoding="utf-8")
+is_english = True
 
 if __name__ == '__main__':
 
-    attack_place = "?"
+    attack_place = ["?", "?"]
 
     attacker_army = []
     defender_army = []
@@ -196,18 +235,27 @@ if __name__ == '__main__':
             break
 
         if "Hunting Field" in line:
-            attack_place = "Hunting Field"
+            attack_place = ["Hunting Field", "Terrain de Chasse"]
+        elif "Terrain de Chasse" in line:
+            attack_place = ["Hunting Field", "Terrain de Chasse"]
+            is_english = False
         elif "Anthill" in line:
-            attack_place = "Anthill"
+            attack_place = ["Anthill", "fourmilière"]
+        elif "fourmilière" in line:
+            attack_place = ["Anthill", "fourmilière"]
+            is_english = False
         elif "Nest" in line:
-            attack_place = "Nest"
+            attack_place = ["Nest", "Loge Impériale"]
+        elif "Loge Impériale" in line:
+            attack_place = ["Nest", "Loge Impériale"]
+            is_english = False
 
-        elif "Attacking troops" in line:
+        elif "Attacking troops" in line or "Troupes en attaque" in line:
             attacker_army = parse_troops(line.split(":")[1])
-        elif "Defending troops" in line:
+        elif "Defending troops" in line or "Troupes en défense" in line:
             defender_army = parse_troops(line.split(":")[1])
 
-        elif "inflict" in line:
+        elif "inflict" in line or "inflige" in line:
             splitted_line = line.split("(")
             second_split = splitted_line[1].split(")")
             splitted_line[1] = second_split[0]
@@ -226,14 +274,23 @@ if __name__ == '__main__':
                 weapons = int(round(bonus_attack / base_attack, 1) * 10)
                 if attacker_weapons == "?":
                     if base_attack < 10:
-                        print("/!\\ Attacker's damages are too low to calculate attacker's weapons level")
+                        if is_english:
+                            print("/!\\ Attacker's base damages are too low (<10) to calculate attacker's Weapons level")
+                        else:
+                            print("/!\\ Les dégâts de base de l'attaquant sont trop faibles (<10) pour calculer le "
+                                  "niveau d'Armes de l'attaquant")
                     else:
                         first_turn_attacker_damages = base_attack + bonus_attack
                         attacker_weapons = weapons
                         first_turn_defender_dead = dead
                 else:
                     if base_attack < 10:
-                        print("/!\\ Defender's damages are too low to calculate defender's weapons level")
+                        if is_english:
+                            print(
+                                "/!\\ Defender's base damages are too low (<10) to calculate defender's weapons level")
+                        else:
+                            print("/!\\ Les dégâts de base du défenseur sont trop faibles (<10) pour calculer le "
+                                  "niveau d'Armes du défenseur")
                     else:
                         first_turn_defender_damages = base_attack + bonus_attack
                         defender_weapons = weapons
@@ -248,32 +305,55 @@ if __name__ == '__main__':
     combat_report.close()
 
     attacker_shields = calculate_attacker_shields()
-    if attack_place == "Hunting Field":
-        defender_shields, ignore = calculate_defender_shields_and_place_level(attack_place)
-    elif attack_place == "Anthill":
-        defender_shields, defender_dome = calculate_defender_shields_and_place_level(attack_place)
-    elif attack_place == "Nest":
-        defender_shields, defender_nest = calculate_defender_shields_and_place_level(attack_place)
+    if attack_place[0] == "Hunting Field":
+        defender_shields, ignore = calculate_defender_shields_and_place_level(attack_place[0])
+    elif attack_place[0] == "Anthill":
+        defender_shields, defender_dome = calculate_defender_shields_and_place_level(attack_place[0])
+    elif attack_place[0] == "Nest":
+        defender_shields, defender_nest = calculate_defender_shields_and_place_level(attack_place[0])
 
     # RESULTS
 
-    print("\nFighting place: " + attack_place)
-    print("Turns: " + str(int(turn)))
+    if is_english:
+        print("\nFighting place: " + attack_place[0])
+        print("Turns: " + str(int(turn)))
 
-    print("\n# Attacker stats:")
-    print("attacker_weapons = " + str(attacker_weapons))
-    print("attacker_shields = " + str(attacker_shields))
+        print("\n# Attacker stats:")
+        print("Weapons = " + str(attacker_weapons))
+        print("Shields = " + str(attacker_shields))
 
-    print("\n# Defender stats:")
-    print("defender_weapons = " + str(defender_weapons))
-    print("defender_shields = " + str(defender_shields))
-    print("defender_dome = " + str(defender_dome))
-    print("defender_nest = " + str(defender_nest))
+        print("\n# Defender stats:")
+        print("Weapons = " + str(defender_weapons))
+        print("Shields = " + str(defender_shields))
+        print("Dome = " + str(defender_dome))
+        print("Nest = " + str(defender_nest))
 
-    print("\n# Attacker's army:")
-    print("Before:", get_readable_army(attacker_army))
-    print("After (no xp!):", get_readable_army(get_remaining_troops(attacker_army, total_attacker_dead)))
+        print("\n# Attacker's army:")
+        print("Before:", get_readable_army(attacker_army))
+        print("After (no xp!):", get_readable_army(get_remaining_troops(attacker_army, total_attacker_dead)))
 
-    print("\n# Defender's army:")
-    print("Before:", get_readable_army(defender_army))
-    print("After (no xp!):", get_readable_army(get_remaining_troops(defender_army, total_defender_dead)))
+        print("\n# Defender's army:")
+        print("Before:", get_readable_army(defender_army))
+        print("After (no xp!):", get_readable_army(get_remaining_troops(defender_army, total_defender_dead)))
+
+    else:
+        print("\nLieu du combat: " + attack_place[1])
+        print("Tours: " + str(int(turn)))
+
+        print("\n# Stats de l'attaquant:")
+        print("Armes = " + str(attacker_weapons))
+        print("Boucliers = " + str(attacker_shields))
+
+        print("\n# Stats du défenseur:")
+        print("Armes = " + str(defender_weapons))
+        print("Boucliers = " + str(defender_shields))
+        print("Dôme = " + str(defender_dome))
+        print("Loge = " + str(defender_nest))
+
+        print("\n# Armée de l'attaquant:")
+        print("Avant:", get_readable_army(attacker_army))
+        print("Après (sans xp !):", get_readable_army(get_remaining_troops(attacker_army, total_attacker_dead)))
+
+        print("\n# Armée du défenseur:")
+        print("Avant:", get_readable_army(defender_army))
+        print("Après (sans xp !):", get_readable_army(get_remaining_troops(defender_army, total_defender_dead)))
